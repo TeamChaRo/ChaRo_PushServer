f2c291500ed7126a52a07bcb973e423fa8d2f942
yaeoni <tjghwnd103@gmail.com>
2021-09-06 15:29:15 +0900

    [FEAT] DB 연결 / push 메시지 생성 및 DB 삽입

--- consumer.ts
+++ consumer.ts
@@ -1,9 +1,31 @@
 import amqp from "amqplib/callback_api";
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+import makeMessage from "./makeMessage";
+
+const envFound = dotenv.config();
+if (envFound.error) {
+  // This error should crash whole process
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
 amqp.connect("amqp://127.0.0.1", function (err, conn) {
   if (err) {
     throw err;
   }
+
+  connection.connect((err) => {
+    if (err) console.log(err);
+    console.log("connect successfully");
+  });
 
   // General push 관리
   conn.createChannel(function (err1, channel) {
@@ -25,18 +47,33 @@ amqp.connect("amqp://127.0.0.1", function (err, conn) {
         if (err2) {
           throw err2;
         }
-        console.log(" [General] Waiting for logs. To exit press CTRL+C");
+        console.log("[General] Waiting for logs. To exit press CTRL+C");
 
         channel.bindQueue(q.queue, exchange, "general");
 
         channel.consume(
           q.queue,
           (msg) => {
-            console.log(
-              " [General] %s: '%s'",
-              msg.fields.routingKey,
-              msg.content.toString()
-            );
+            const date = new Date()
+              .toISOString()
+              .slice(0, 19)
+              .replace("T", " ");
+
+            let key = msg.fields.routingKey;
+            msg = JSON.parse(msg.content.toString());
+
+            console.log("[General] %s: ", key, msg);
+
+            //PUSH db 삽입
+            const query = `INSERT INTO push(pushCode, image, token, createdAt) VALUES(${msg.pushCode}, '${msg.image}', '${msg.token}', '${date}');`;
+            connection.query(query, (err, results, field) => {
+              if (err) console.log(err);
+
+              const push = makeMessage(msg, results.insertId, date);
+              console.log(push);
+
+              // 여기서 firebase-admin 연결해서 전송
+            });
           },
           {
             noAck: true,
@@ -75,4 +112,10 @@ amqp.connect("amqp://127.0.0.1", function (err, conn) {
       }
     );
   });
+});
+
+process.on("SIGINT", function () {
+  console.log("Caught interrupt signal");
+  connection.end();
+  process.exit(0);
 });
---
+++ makeMessage.ts
@@ -0,0 +1,27 @@
+export default function (msg: Object, id: number, date: string): Object {
+  const code = msg["pushCode"];
+  let push = {
+    data: {
+      push_code: code,
+      push_id: id,
+      image: msg["image"],
+      token: msg["token"],
+      date: date,
+      title: "",
+      body: "",
+    },
+    token: msg["to"],
+  };
+
+  if (code == 0) {
+    push.data.title = "팔로우한 사람 게시글 올림";
+    push.data.body = "게시글 올림 바디";
+  } else if (code == 1) {
+    push.data.title = "누가 나를 팔로우";
+    push.data.body = "누가나를 파로우 바디";
+  } else if (code == 2) {
+    push.data.title = "내 게시물 좋아여";
+    push.data.body = "좋아여 바디";
+  }
+  return push;
+}
